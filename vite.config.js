e58b37034e07e453b44839a3d2355263ecd4a4a1
coffee-and <cheeseee.bagel@gmail.com>
2026-07-17 01:59:39 +0900

    Configure GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isGitHubPagesBuild = process.env.GITHUB_PAGES === 'true';
+
 export default defineConfig({
+  base: isGitHubPagesBuild ? '/moment-play/' : '/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
