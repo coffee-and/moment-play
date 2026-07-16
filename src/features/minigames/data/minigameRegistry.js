51ed0d0b905d10548fafb0b6b0fdc9714dd38d58
coffee-and <cheeseee.bagel@gmail.com>
2026-07-17 00:50:14 +0900

    feat: register snake and timing games

--- src/features/minigames/data/minigameRegistry.js
+++ src/features/minigames/data/minigameRegistry.js
@@ -2,12 +2,16 @@ import { Game2048 } from "../games/game-2048/Game2048.jsx";
 import { MemoryOrderGame } from "../games/memory/MemoryOrderGame.jsx";
 import { OmokGame } from "../games/omok/OmokGame.jsx";
 import { SudokuLevelGame } from "../games/sudoku/SudokuLevelGame.jsx";
+import { SnakeGame } from "../games/snake/SnakeGame.jsx";
+import { TimingTapGame } from "../games/timing-tap/TimingTapGame.jsx";
 
 export const MINIGAME_COMPONENTS = {
   "2048": Game2048,
   memory: MemoryOrderGame,
   sudoku: SudokuLevelGame,
   omok: OmokGame,
+  snake: SnakeGame,
+  "timing-tap": TimingTapGame,
 };
 
 export function getMinigameComponent(gameId) {
