51f709a5ffec8b68116c5532d626121cc4fed235
coffee-and <cheeseee.bagel@gmail.com>
2026-07-26 23:13:26 +0900

    별빛비행 물고기 지느러미 형태 수정

--- src/features/minigames/games/flappy/FlappyFish.jsx
+++ src/features/minigames/games/flappy/FlappyFish.jsx
@@ -13,7 +13,7 @@ export function FlappyFish({ className = "" }) {
         viewBox="0 0 160 160"
       >
         <path
-          d="M48 144A32 32 0 0 1 48 80A32 32 0 0 1 48 16Q99.2 16 144 80Q99.2 144 48 144Z"
+          d="M54 134A27 27 0 0 1 54 80A27 27 0 0 1 54 26Q96 26 138 80Q96 134 54 134Z"
           fill="var(--fish-tail-color)"
           stroke="var(--fish-outline-color)"
           strokeLinejoin="round"
@@ -38,10 +38,10 @@ export function FlappyFish({ className = "" }) {
         className="flappy-fish-svg__wing"
         focusable="false"
         preserveAspectRatio="none"
-        viewBox="0 0 190 190"
+        viewBox="0 0 180 150"
       >
         <path
-          d="M57 171A38 38 0 0 1 57 95A38 38 0 0 1 57 19Q117.8 19 171 95Q117.8 171 57 171Z"
+          d="M24 75C24 43 54 19 92 19C123 19 149 39 160 75C149 111 123 131 92 131C54 131 24 107 24 75Z"
           fill="var(--fish-fin-color)"
           stroke="var(--fish-outline-color)"
           strokeLinejoin="round"
